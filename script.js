456815ca9a2d2d78c5fae189b050781545d96469
shimizu1026 <115125150+shimizu1026@users.noreply.github.com>
2022-11-23 16:11:50 +0900

    index common script

--- script.js
+++ script.js
@@ -24,31 +24,31 @@ function fadeAnime(){
 
     //ふわっと動くきっかけのクラス名と動きのクラス名の設定
     $('.fadeUpTrigger').each(function(){ //fadeInUpTriggerというクラス名が
-    　　var elemPos = $(this).offset().top-50; //要素より、50px上の
-    　　var scroll = $(window).scrollTop();
-    　　var windowHeight = $(window).height();
-    　　if (scroll >= elemPos - windowHeight){
-    　　$(this).addClass('fadeUp');
-    　　// 画面内に入ったらfadeInというクラス名を追記
-    　　}else{
-    　　　$(this).removeClass('fadeUp');
-    　　// 画面外に出たらfadeInというクラス名を外す
-    　　}
-    　　});
+    var elemPos = $(this).offset().top-50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeUp');
+    // 画面内に入ったらfadeInというクラス名を追記
+    }else{
+    $(this).removeClass('fadeUp');
+    // 画面外に出たらfadeInというクラス名を外す
+    }
+    });
     
     //関数でまとめることでこの後に違う動きを追加することが出来ます
     $('.fadeDownTrigger').each(function(){ //fadeInDownTriggerというクラス名が
-    　　var elemPos = $(this).offset().top-50; //要素より、50px上の
-    　　var scroll = $(window).scrollTop();
-    　　var windowHeight = $(window).height();
-    　　if (scroll >= elemPos - windowHeight){
-    　　$(this).addClass('fadeDown');
-    　　// 画面内に入ったらfadeDownというクラス名を追記
-    　　}else{
-    　　　$(this).removeClass('fadeDown');
-    　　// 画面外に出たらfadeDownというクラス名を外す
-    　　}
-    　　});
+    var elemPos = $(this).offset().top-50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeDown');
+    // 画面内に入ったらfadeDownというクラス名を追記
+    }else{
+    $(this).removeClass('fadeDown');
+    // 画面外に出たらfadeDownというクラス名を外す
+    }
+    });
     
     
     }//ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
