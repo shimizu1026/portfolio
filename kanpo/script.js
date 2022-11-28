353b12f25f1e32d3de9cd1d33cb11fd94469b1b2
shimizu1026 <115125150+shimizu1026@users.noreply.github.com>
2022-11-28 11:39:38 +0900

    kanpo

---
+++ kanpo/script.js
@@ -0,0 +1,84 @@
+'use strict';
+// 動きのきっかけの起点となるアニメーションの名前を定義
+function blurAnime(){
+
+  // じわっ
+  
+  $('.blurTrigger').each(function(){ //blurTriggerというクラス名が
+    var elemPos = $(this).offset().top-50;//要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('blur');// 画面内に入ったらblurというクラス名を追記
+    }else{
+    $(this).removeClass('blur');// 画面外に出たらblurというクラス名を外す
+    }
+    });
+
+}
+    // 画面をスクロールをしたら動かしたい場合の記述
+    $(window).scroll(function (){
+        blurAnime();/* アニメーション用の関数を呼ぶ*/
+      });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+  
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+    //ふわっと動くきっかけのクラス名と動きのクラス名の設定
+    $('.fadeUpTrigger').each(function(){ //fadeInUpTriggerというクラス名が
+    var elemPos = $(this).offset().top-50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeUp');
+    // 画面内に入ったらfadeInというクラス名を追記
+    }else{
+    $(this).removeClass('fadeUp');
+    // 画面外に出たらfadeInというクラス名を外す
+    }
+    });
+    
+    //関数でまとめることでこの後に違う動きを追加することが出来ます
+    $('.fadeDownTrigger').each(function(){ //fadeInDownTriggerというクラス名が
+    var elemPos = $(this).offset().top-50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeDown');
+    // 画面内に入ったらfadeDownというクラス名を追記
+    }else{
+    $(this).removeClass('fadeDown');
+    // 画面外に出たらfadeDownというクラス名を外す
+    }
+    });
+    
+    
+    }
+    //ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
+
+    // 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function (){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+
+  //トップに戻るボタンの設定
+//スクロールイベント
+window.onscroll = function() {
+  //現在位置（スクロール値）の取得
+  const scrollPosition = window.pageYOffset;
+
+  console.log(scrollPosition);
+
+      //scrollPositionの値が600以上の時は#page-topに.openを追加
+      
+  if (scrollPosition >= 600) {
+      document.getElementById('page-top').classList.add('open');
+  }
+      //それ以外の時は#page-topから.openを削除
+  else {
+      document.getElementById('page-top').classList.remove('open');
+  }
+};
+
+
